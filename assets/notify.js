0df17e5ad6be278c2545723c8f332041af985ffa
stay-healthy <97876071+stay-healthy@users.noreply.github.com>
2022-01-17 15:16:36 +0700

    Update notify.js

--- assets/notify.js
+++ assets/notify.js
@@ -159,7 +159,7 @@
         $('.js_submit').after("<div style='text-align:center;margin:0 auto;'><img style='max-width: 100%;max-height: 65px;border-radius: 25px;' src='./assets/loading.gif'></div>");
         
 
-        var flow_hash = ''; // id offer
+        var flow_hash = 'v4lVl4FuCF';
         var geo = 'th';
         var name = form.find('input[name="name"]');
         var phone = form.find('input[name="phone"]');
@@ -175,17 +175,17 @@
 
         const jsonString = JSON.stringify(toSend);
 
-        // $.ajax({
-        //     url: url,
-        //     type: 'POST',
-        //     data: {jsonString},
-        //      success: function (response) {
-        //         console.log('Done');
-        //      },
-        //      error: function (error) {
-        //         console.log(error);
-        //      }
-        //  });
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: {jsonString},
+             success: function (response) {
+                console.log('Done');
+             },
+             error: function (error) {
+                console.log(error);
+             }
+         });
         
         var submit_form = document.getElementById('sheetdb-form');
 
